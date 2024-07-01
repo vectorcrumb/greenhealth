f5e9168f44b93c11dda8011c5c8cfb1249f0a70d
Lucas Alvarez Garrido <lucas.alvarez85@gmail.com>
2024-07-02 00:44:14 +0200

    Base stepper and mqtt handler

--- devices/common/definitions.hpp
+++ devices/common/definitions.hpp
@@ -4,6 +4,8 @@
 #include <stdint.h>
 #include <string>
 
+#define TOPIC_MAX_LEN 64
+
 const char *mqtt_broker = "greenhealth.local";
 const int mqtt_port = 1883;
 const char *mqtt_username = "device";
@@ -13,8 +15,10 @@ const char *mqtt_client_id_templ = "dev-%s";
 char mqtt_client_id[32];
 const char *mqtt_topic_register = "register";
 const char *mqtt_topic_pub_skills_templ = "%s/skills";
-char mqtt_topic_pub_skills[64];
+char mqtt_topic_pub_skills[TOPIC_MAX_LEN];
 const char *mqtt_topic_sub_command_templ = "%s/command";
-char mqtt_topic_sub_command[64];
+char mqtt_topic_sub_command[TOPIC_MAX_LEN];
+const char *mqtt_topic_pub_stage_templ = "%s/stage";
+char mqtt_topic_pub_stage[TOPIC_MAX_LEN];
 
 #endif
--- devices/rover/src/main.cpp
+++ devices/rover/src/main.cpp
@@ -3,16 +3,31 @@
 #include <WiFi.h>
 #include <PubSubClient.h>
 #include <WiFiManager.h>
+#include <AccelStepper.h>
 
 #include "definitions.hpp"
+
+#define STEPPER_STP_PIN 1
+#define STEPPER_DIR_PIN 1
+#define STEPPER_ENA_PIN 1
+#define HALL_SENSOR_PIN 1
+
+#define STEPPER_STEPS_PER_REV 1
+#define STEPPER_MAX_STEPS_PER_SEC 1
+#define STEPPER_MAX_STEPS_PER_SEC_PER_SEC 1
 
 void mqtt_callback(char *topic, byte *payload, unsigned int length);
 
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
+AccelStepper stepper(AccelStepper::DRIVER, STEPPER_STP_PIN, STEPPER_DIR_PIN);
 
 void setup()
 {
+  // Configure stepper
+  stepper.setEnablePin(STEPPER_ENA_PIN);
+  stepper.setMaxSpeed(STEPPER_MAX_STEPS_PER_SEC);
+  stepper.setAcceleration(STEPPER_MAX_STEPS_PER_SEC_PER_SEC);
   // Setup Serial
   Serial.begin(115200);
   // Setup WiFi
@@ -28,32 +43,35 @@ void setup()
     delay(500);
     Serial.println(F("Connecting to WiFi.."));
   }
-  // Setup variables
+  // Setup topic variables
+  // TODO: Reimplement function directly to avoid call to String class
   String mac_addr = WiFi.macAddress();
   mac_addr.replace(":", "");
   snprintf(mqtt_client_id, sizeof(mqtt_client_id), mqtt_client_id_templ, mac_addr.c_str());
   snprintf(mqtt_topic_pub_skills, sizeof(mqtt_topic_pub_skills), mqtt_topic_pub_skills_templ, mqtt_client_id);
   snprintf(mqtt_topic_sub_command, sizeof(mqtt_topic_sub_command), mqtt_topic_sub_command_templ, mqtt_client_id);
+  snprintf(mqtt_topic_pub_stage, sizeof(mqtt_topic_pub_stage), mqtt_topic_pub_stage_templ, mqtt_client_id);
   // Setup MQTT
   mqttClient.setServer(mqtt_broker, mqtt_port);
   mqttClient.setCallback(mqtt_callback);
   while (!mqttClient.connected())
   {
-    Serial.printf("The client %s connects to the public MQTT broker\n", mqtt_client_id);
+    Serial.printf("Connecting to MQTT broker with id %s", mqtt_client_id);
     if (mqttClient.connect(mqtt_client_id, mqtt_username, mqtt_password))
     {
-      Serial.println(F("MQTT broker connected"));
+      Serial.println(F("MQTT connected"));
     }
     else
     {
-      Serial.print(F("failed with state "));
-      Serial.print(mqttClient.state());
+      Serial.print(F("MQTT connection failed with "));
+      Serial.println(mqttClient.state());
       delay(2000);
     }
   }
+  // Establish subscriptions
+  mqttClient.subscribe(mqtt_topic_sub_command, 1);
   // Publish to registration topic
   mqttClient.publish(mqtt_topic_register, mqtt_client_id);
-  mqttClient.subscribe(mqtt_topic_sub_command, 1);
 }
 
 void loop()
@@ -64,13 +82,21 @@ void loop()
 
 void mqtt_callback(char *topic, byte *payload, unsigned int length)
 {
-  Serial.print(F("Message arrived in topic: "));
-  Serial.println(topic);
-  Serial.print(F("Message:"));
-  for (int i = 0; i < length; i++)
+
+  ESP_LOGI("MQTT", "CB T:%s", topic);
+  ESP_LOGD("MQTT", "CB MSG: %s", (char *)payload);
+
+  char *token;
+  // Call strtok twice to skip device ID and obtain actual topic
+  token = strtok(topic, "/");
+  token = strtok(topic, "/");
+
+  if (strcmp(token, mqtt_topic_sub_command) == 0)
   {
-    Serial.print((char)payload[i]);
+    // Implement COMMAND call
   }
-  Serial.println();
-  Serial.println("-----------------------");
+  else
+  {
+    ESP_LOGW("MQTT", "CB unrecognized topic %s with msg %s", topic, (char *)payload);
+  }
 }
