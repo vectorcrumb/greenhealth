b2326045f6eb486fead994a54fedeb5a10f172f5
Lucas Alvarez Garrido <lucas.alvarez85@gmail.com>
2024-07-08 02:16:54 +0200

    More configs and mqtt_pub can publish

--- backend/app/src/mqtt_pub.py
+++ backend/app/src/mqtt_pub.py
@@ -1,11 +1,20 @@
 import threading
 import paho.mqtt.client as paho
 import queue
-from typing import Any
+from typing import Any, Dict
 import sentinel
 import logging
+from dataclasses import dataclass
+
 
 ThreadEnd = sentinel.create("ThreadEnd")
+
+
+@dataclass
+class MQTTPublishable:
+    topic: str
+    message: str
+    qos: int = 1
 
 
 class MQTTPublisher(threading.Thread):
@@ -14,9 +23,15 @@ class MQTTPublisher(threading.Thread):
         self._queue = queue.Queue()
         self.mqtt_client = mqtt_client
         self.daemon = True
+        self.mqtt_lock = threading.Lock()
         logging.debug("MQTT Publisher thread ready")
 
-    def enqueue(self, msg: Any):
+    def _enqueue(self, msg: Any):
+        self._queue.put(msg)
+
+    def enqueue(self, msg: MQTTPublishable):
+        if not isinstance(msg, MQTTPublishable):
+            raise ValueError("Passed msg argument is not an MQTTPublishable type")
         self._queue.put(msg)
 
     def teardown(self):
@@ -28,3 +43,6 @@ class MQTTPublisher(threading.Thread):
             logging.debug(f"Queue elem: {qelem}")
             if qelem is ThreadEnd:
                 break
+            if isinstance(qelem, MQTTPublishable):
+                with self.mqtt_lock:
+                    self.mqtt_client.publish(qelem.topic, qelem.message, qelem.qos)
--- backend/app/src/utils.py
+++ backend/app/src/utils.py
@@ -2,6 +2,13 @@ import json
 from typing import Dict
 from definitions import *
 from collections import defaultdict
+import secrets
+import string
+
+
+def generate_random_id(length: int = 8) -> str:
+    alphabet = string.ascii_lowercase + string.digits
+    return "".join(secrets.choice(alphabet) for _ in range(length))
 
 
 # TODO: Change this to a schema based validator!
