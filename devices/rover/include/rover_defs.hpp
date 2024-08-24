6a1bc89099724a8b07a990c306cd7613550d38c7
Lucas Alvarez Garrido <lucas.alvarez85@gmail.com>
2024-08-24 15:48:25 +0200

    Complete rover code

--- devices/rover/include/rover_defs.hpp
+++ devices/rover/include/rover_defs.hpp
@@ -2,11 +2,17 @@
 #define ROVER_DEFS_HPP_
 
 enum RoverAction {
-  NONE,
-  STANDBY,
-  MOVE_MM,
-  MOVE_TO_NEXT_NODE,
-  MOVE_N_NODES,
+  NONE = 0,
+  STANDBY = 1,
+  MOVE_MM = 2,
+  MOVE_TO_NEXT_NODE = 3,
+  MOVE_N_NODES = 4,
+};
+
+struct CurrentAction {
+  RoverAction action;
+  int parameter;
+  int timeout_ms;
 };
 
 #endif
--- devices/rover/src/main.cpp
+++ devices/rover/src/main.cpp
@@ -1,8 +1,11 @@
 #include <AccelStepper.h>
 #include <Arduino.h>
+#include <ArduinoJson.h>
 #include <MQTT.h>
 #include <WiFi.h>
 #include <WiFiManager.h>
+
+#include <atomic>
 
 #include "definitions.hpp"
 #include "rover_defs.hpp"
@@ -16,33 +19,50 @@
 #define STEPPER_STEPS_PER_REV 1
 #define STEPPER_MAX_STEPS_PER_SEC 1
 #define STEPPER_MAX_STEPS_PER_SEC_PER_SEC 1
+#define STEPPER_SPEED 1
 
 #define MUTEX_STATE_TIMEOUT 25
-#define COMMAND_FEEDBACK_PERIOD 200
-#define COMMAND_FEEDBACK_PERIOD_TICKS pdMS_TO_TICKS(COMMAND_FEEDBACK_PERIOD)
+#define COMMAND_FEEDBACK_PERIOD_MS 200
+#define COMMAND_FEEDBACK_PERIOD_TICKS pdMS_TO_TICKS(COMMAND_FEEDBACK_PERIOD_MS)
+#define MAIN_LOOP_PERIOD_MS 20
+#define MAIN_LOOP_PERIOD_TICKS pdMS_TO_TICKS(MAIN_LOOP_PERIOD_MS)
 
-void mqtt_callback(char *topic, byte *payload, unsigned int length);
+void mqtt_callback(MQTTClient *client, char *topic, char *payload, int length);
 
 WiFiClient wifiClient;
 MQTTClient mqttClient;
-// PubSubClient mqttClient(wifiClient);
 AccelStepper stepper(AccelStepper::DRIVER, STEPPER_STP_PIN, STEPPER_DIR_PIN);
-RoverAction current_action = RoverAction::NONE;
+
 StaticSemaphore_t mutex_buffer_state;
 SemaphoreHandle_t mutex_state;
-bool connection_established = false;
-bool action_in_progress = false;
-bool action_completed = false;
+
+volatile CurrentAction current_action;
+std::atomic_bool connection_established;
+std::atomic_bool action_in_progress;
+std::atomic_bool action_completed;
+volatile std::atomic_int32_t nodes_detected;
+volatile std::atomic_bool detected_hall_sensor;
 long action_start_time = 0;
 TickType_t action_feedback_time_marker = 0;
 
+void IRAM_ATTR hall_isr() {
+  detected_hall_sensor = true;
+}
+
 void setup() {
+  connection_established = false;
+  action_in_progress = false;
+  action_completed = false;
+  detected_hall_sensor = false;
+  nodes_detected = 0;
   // Create mutex
   mutex_state = xSemaphoreCreateMutexStatic(&mutex_buffer_state);
   // Configure stepper
   stepper.setEnablePin(STEPPER_ENA_PIN);
   stepper.setMaxSpeed(STEPPER_MAX_STEPS_PER_SEC);
   stepper.setAcceleration(STEPPER_MAX_STEPS_PER_SEC_PER_SEC);
+  // Configure sensor. Configure as INPUT or INPUT_PULLUP accordingly to sensor
+  pinMode(HALL_SENSOR_PIN, INPUT);
   // Setup Serial
   Serial.begin(115200);
   // Setup WiFi
@@ -83,31 +103,98 @@ void setup() {
   mqttClient.subscribe(mqtt_client_id, 1);
   // Publish to registration topic
   mqttClient.publish(mqtt_topic_register, mqtt_client_id, false, 1);
+  // Attach the hardware interrupt to the hall sensor. Configure as RISING or FALLING
+  attachInterrupt(digitalPinToInterrupt(HALL_SENSOR_PIN), hall_isr, FALLING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  TickType_t start_loop_time = xTaskGetTickCount();
+  // Let's copy the current state variables so that other tasks don't overwrite these during the loop
+  bool action_completed_current, action_in_progress_current;
+  if (xSemaphoreTake(mutex_state, pdMS_TO_TICKS(MUTEX_STATE_TIMEOUT)) == pdTRUE) {
+    action_completed_current = action_completed;
+    action_in_progress_current = action_in_progress;
+  } else {
+    // If we can't sample the state variables, skip this iteration
+    ESP_LOGE("MUTEX", "COULD NOT CAPTURE MUTEX @ %s:%d", __func__, __LINE__);
+    return;
+  }
+  // Loop the mqtt client to handling incoming messages
   mqttClient.loop();
-  if (action_completed && action_in_progress) {
+  // Loop the motor
+  stepper.run();
+  // Detect if we've just finished a previously in progress action
+  if (action_completed_current && action_in_progress_current) {
+    // Publish the completion to the result topic with how long it took to complete
     long total_time_ms = pdTICKS_TO_MS(xTaskGetTickCount()) - action_start_time;
     snprintf(mqtt_msg_cmd_res, sizeof(mqtt_msg_cmd_res), mqtt_msg_cmd_res_templ, btoa(true), total_time_ms);
     mqttClient.publish(mqtt_topic_cmd_res, mqtt_msg_cmd_res, false, 1);
-    action_in_progress = false;
+    // Reset state. Delete the current action!
     action_completed = true;
-  } else if (action_in_progress) {
+    action_in_progress = false;
+    current_action.action = RoverAction::STANDBY;
+  }
+  // Detect if we just started an action to effect a command on the stepper motor
+  else if (current_action.action > RoverAction::STANDBY && !(action_completed_current && action_in_progress_current)) {
+    // Start an action
+    action_in_progress = true;
+    if (stepper.isRunning()) {
+      ESP_LOGI("MAIN", "Stepper is already running!");
+    }
+    // Decide what to do
+    else if (current_action.action == RoverAction::MOVE_MM) {
+      stepper.move(current_action.parameter);
+    } else if (current_action.action == RoverAction::MOVE_TO_NEXT_NODE) {
+      stepper.setSpeed(STEPPER_SPEED);
+    } else if (current_action.action == RoverAction::MOVE_N_NODES) {
+      stepper.setSpeed(STEPPER_SPEED);
+    }
+  }
+  // Detect if we're progressing an action to publish feedback
+  else if (action_in_progress_current) {
+    // If enough time has elapsed for an update, compute current running time and publish
     if (xTaskGetTickCount() - action_feedback_time_marker > COMMAND_FEEDBACK_PERIOD_TICKS) {
       long current_time_ms = pdTICKS_TO_MS(xTaskGetTickCount()) - action_start_time;
       snprintf(MSG(cmd_fb), sizeof(MSG(cmd_fb)), MSG_TEMPL(cmd_fb), current_time_ms);
       mqttClient.publish(TOPIC(cmd_fb), MSG(cmd_fb), false, 1);
       action_feedback_time_marker = xTaskGetTickCount();
     }
-
-    // if (current_action == RoverAction::MOVE_MM) {
-
-    // }
+    // Check actions one by one
+    // move_mm will finish automatically, let's end the action if we're done moving
+    if (current_action.action == RoverAction::MOVE_MM) {
+      if (!stepper.isRunning()) {
+        action_completed = true;
+      }
+    }
+    // move_to_next_node finishes on detection of hall effect sensor
+    else if (current_action.action == RoverAction::MOVE_TO_NEXT_NODE) {
+      if (detected_hall_sensor) {
+        stepper.stop();
+        action_completed = true;
+        detected_hall_sensor = false;
+      }
+    }
+    // move_n_nodes finishes on sensor + having detected n nodes
+    else if (current_action.action == RoverAction::MOVE_N_NODES) {
+      if (detected_hall_sensor) {
+        nodes_detected++;
+        // Check if we've reached the n nodes
+        if (current_action.parameter - nodes_detected == 0) {
+          stepper.stop();
+          action_completed = true;
+          detected_hall_sensor = false;
+          nodes_detected = 0;
+        }
+      }
+    }
   }
-
-  delay(5);
+  // We determine the remaining loop time
+  TickType_t loop_time = xTaskGetTickCount() - start_loop_time;
+  if (loop_time < MAIN_LOOP_PERIOD_TICKS) {
+    vTaskDelay(MAIN_LOOP_PERIOD_TICKS - loop_time);
+  } else {
+    ESP_LOGW("MAIN", "Loop time: %d ticks. Expected loop time: %d ticks", loop_time, MAIN_LOOP_PERIOD_TICKS);
+  }
 }
 
 void mqtt_callback(MQTTClient *client, char topic[], char payload[], int length) {
@@ -129,13 +216,30 @@ void mqtt_callback(MQTTClient *client, char topic[], char payload[], int length)
       token = strtok(topic, "/");
       // Check if action is registered and if so, update current action
       if (strcmp(token, "move_mm") == 0) {
-        current_action = RoverAction::MOVE_MM;
+        // Retrieve the mm and timeout_ms parameters from the move_mm payload
+        JsonDocument doc;
+        deserializeJson(doc, payload, length);
+        current_action.parameter = doc["mm"];
+        current_action.timeout_ms = doc["timeout_ms"];
+        current_action.action = RoverAction::MOVE_MM;
       } else if (strcmp(token, "move_to_next_node") == 0) {
-        current_action = RoverAction::MOVE_TO_NEXT_NODE;
+        // Retrieve the timeout_ms parameters from the move_to_next_node payload
+        JsonDocument doc;
+        deserializeJson(doc, payload, length);
+        current_action.parameter = 0;
+        current_action.timeout_ms = doc["timeout_ms"];
+        current_action.action = RoverAction::MOVE_TO_NEXT_NODE;
       } else if (strcmp(token, "move_n_nodes") == 0) {
-        current_action = RoverAction::MOVE_N_NODES;
+        // Retrieve the n and timeout_ms parameters from the move_n_nodes payload
+        JsonDocument doc;
+        deserializeJson(doc, payload, length);
+        current_action.parameter = doc["n"];
+        current_action.timeout_ms = doc["timeout_ms"];
+        current_action.action = RoverAction::MOVE_N_NODES;
       }
-      action_in_progress = true;
+      // Configure state
+      action_in_progress = false;
+      action_completed = false;
       action_start_time = pdTICKS_TO_MS(xTaskGetTickCount());
       xSemaphoreGive(mutex_state);
     } else {
